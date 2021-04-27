b8554248a9ca90eff9913256ccf6a83a954c43b2
Yizhen-Zhao <54888697+Yizhen-Zhao@users.noreply.github.com>
2021-04-27 09:26:55 +0200

    Update run_spark.sh

--- run_spark.sh
+++ run_spark.sh
@@ -3,4 +3,4 @@
 set -e
 
 
-etc/spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.3,org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.3 ./src/kpmg_case/process/consumer.py
+etc/spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.3,org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.3 ./src/case_study/process/consumer.py
